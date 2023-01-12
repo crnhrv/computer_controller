bb2a4f9b09612dfbc730d8abff0c1d1dce2006b9
Ciaran Harvey <ciaran.harvey@red-gate.com>
2023-01-12 00:05:08 +0000

    Add support for long pressing buttons (repeated keysends)

--- client/lib/widgets/keyboard_control.dart
+++ client/lib/widgets/keyboard_control.dart
@@ -1,48 +1,83 @@
 import 'package:flutter/material.dart';
 import '../models/keyboard_control_mode.dart';
 
-class KeyboardControl extends StatelessWidget {
+class KeyboardControl extends StatefulWidget {
   final Function(int keyCode) sendCommand;
   const KeyboardControl({Key? key, required this.sendCommand})
       : super(key: key);
 
-  List<Widget> _getTextKeyBlocks(List<String> keys, Color color, double textSize) {
-    return List<Widget>.generate(
-        keys.length,
-            (index) => Container(
-          width: 80,
-          height: 80,
-          decoration: BoxDecoration(
-              color: color, border: Border.all(width: 1)),
-          child: TextButton(
-              onPressed: () {
-                sendCommand(KeyboardControlMode.getKeyCode(keys[index]));
-              },
-              child: Center(
-                child: Text(keys[index], textAlign: TextAlign.center, style: TextStyle(fontSize: textSize)),
-              )),
-        ));
-  }
+  @override
+  State<KeyboardControl> createState() => _KeyboardControlState();
+}
 
-  List<Widget> _getIconKeyBlocks(List<String> keys, Color color, double iconSize) {
+class _KeyboardControlState extends State<KeyboardControl> {
+  bool _isPressed = false;
+
+  List<Widget> _getTextKeyBlocks(
+      List<String> keys, Color color, double textSize) {
     return List<Widget>.generate(
         keys.length,
         (index) => Container(
               width: 80,
               height: 80,
-              decoration: BoxDecoration(
-                  color: color, border: Border.all(width: 1)),
-          child: IconButton(
-              onPressed: () {
-                sendCommand(KeyboardControlMode.getKeyCode(keys[index]));
-              },
-              icon: Icon(KeyboardControlMode.getIcon(
-                (keys[index]),
+              decoration:
+                  BoxDecoration(color: color, border: Border.all(width: 1)),
+              child: GestureDetector(
+                child: TextButton(
+                    onPressed: () {
+                      widget.sendCommand(
+                          KeyboardControlMode.getKeyCode(keys[index]));
+                    },
+                    child: Center(
+                        child: Text(keys[index],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: textSize)))),
+                onLongPressStart: (_) async {
+                  _isPressed = true;
+                  do {
+                    widget.sendCommand(
+                        KeyboardControlMode.getKeyCode(keys[index]));
+                    await Future.delayed(
+                        const Duration(milliseconds: 100), () {});
+                  } while (_isPressed);
+                },
+                onLongPressEnd: (_) => setState(() => _isPressed = false),
               ),
-              size: iconSize)),
-        ));
+            ));
   }
 
+  List<Widget> _getIconKeyBlocks(
+      List<String> keys, Color color, double iconSize) {
+    return List<Widget>.generate(
+        keys.length,
+        (index) => Container(
+              width: 80,
+              height: 80,
+              decoration:
+                  BoxDecoration(color: color, border: Border.all(width: 1)),
+              child: GestureDetector(
+                child: IconButton(
+                    iconSize: iconSize,
+                    onPressed: () {
+                      widget.sendCommand(
+                          KeyboardControlMode.getKeyCode(keys[index]));
+                    },
+                    icon: Icon(KeyboardControlMode.getIcon(
+                      (keys[index]),
+                    ))),
+                onLongPressStart: (_) async {
+                  _isPressed = true;
+                  do {
+                    widget.sendCommand(
+                        KeyboardControlMode.getKeyCode(keys[index]));
+                    await Future.delayed(
+                        const Duration(milliseconds: 100), () {});
+                  } while (_isPressed);
+                },
+                onLongPressEnd: (_) => setState(() => _isPressed = false),
+              ),
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,21 +86,26 @@ class KeyboardControl extends StatelessWidget {
       children: [
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: _getTextKeyBlocks(KeyboardControlMode.getPgKeys(), Colors.orange, 18)),
+            children: _getTextKeyBlocks(
+                KeyboardControlMode.getPgKeys(), Colors.orange, 18)),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: _getTextKeyBlocks(KeyboardControlMode.getRegularKeys(), Colors.red, 20)),
+            children: _getTextKeyBlocks(
+                KeyboardControlMode.getRegularKeys(), Colors.red, 20)),
         Column(children: [
           Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: _getIconKeyBlocks(KeyboardControlMode.getMediaKeys(), Colors.green, 36)),
+              children: _getIconKeyBlocks(
+                  KeyboardControlMode.getMediaKeys(), Colors.green, 36)),
           Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: _getIconKeyBlocks(KeyboardControlMode.getVolumeKeys(), Colors.green, 36))
+              children: _getIconKeyBlocks(
+                  KeyboardControlMode.getVolumeKeys(), Colors.green, 36))
         ]),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: _getIconKeyBlocks(KeyboardControlMode.getArrowKeys(), Colors.red, 36))
+            children: _getIconKeyBlocks(
+                KeyboardControlMode.getArrowKeys(), Colors.red, 36))
       ],
     );
   }
